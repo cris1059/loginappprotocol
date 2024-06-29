50ab7a635333c1bc058f9ef9d55f784f6128e25c
cris1059 <149038713+cris1059@users.noreply.github.com>
2024-06-29 01:21:56 -0600

    Update loadnav.js

--- assets/js/loadnav.js
+++ assets/js/loadnav.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#navbar-container").load("../pages/requieres/nav_bar.html", function() {
+    $("#navbar-container").load("../requieres/nav_bar.html", function() {
         // Se asegura de que la funcionalidad de la barra de navegación se cargue
         $('#menu-icon').click(function() {
             $('#nav-menu').toggleClass('active');
